9b5d557d7f8d6c1dba77f82f4beb561ff4cff324
Courey J <coureyfrance@gmail.com>
2026-09-17 13:57:31 -0400

    Added a new record (Mac details)

---
+++ insert_record.py
@@ -0,0 +1,45 @@
+import mysql.connector as mydbconnection
+from mysql.connector import Error
+
+#modular means it's easy to change and move around, plug and play, can stand alone
+def connect():
+    conn = None
+
+    try:
+        conn = mydbconnection.connect(
+            database='usersdb',
+            user='root',
+            password='Orange47'
+        )
+
+        if conn.is_connected():
+            print('✅ Connected to database')
+
+        # Creates a cursor object that allows SQL actions to the MySQL server engine
+        cursor = conn.cursor()
+
+        # Create a SQL Query we want to run
+        query = '''
+            INSERT INTO laptop (ID, Name, Price, Purchase_Date)
+                VALUES(13, 'Mac Air', 1000, '2026-05-13')
+        '''
+        # Execute the query in SQL engine/server
+        cursor.execute(query)
+        print('✅ Query Executed')
+
+        # Have to commit the query to actually send the data/table update
+        conn.commit()
+        print('✅ Transaction Committed')
+
+        print(f'{cursor.rowcount}: Record inserted successfully 🎉')
+
+    except Error as e:
+        print(f'❌ Error: {e}')
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+            print('Connection Closed')
+
+if __name__ == "__main__":
+    connect()
