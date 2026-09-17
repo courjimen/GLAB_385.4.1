b11b215829b7325948e811618511444ce0688cfd
Courey J <coureyfrance@gmail.com>
2026-09-17 13:35:44 -0400

    Created table 'Laptop'

--- create_table.py
+++ create_table.py
@@ -15,6 +15,23 @@ def connect():
         if conn.is_connected():
             print('✅ Connected to database')
 
+        # Creates a cursor object that allows SQL actions to the MySQL server engine
+        cursor = conn.cursor()
+
+        # Create a SQL Query we want to run
+        query = '''
+            CREATE TABLE laptop (
+                ID int(11) NOT NULL,
+                Name varchar(250) NOT NULL,
+                Price float NOT NULL,
+                Purchase_Date date NOT NULL
+            )
+        '''
+
+        cursor.execute(query)
+
+        print('✅ Created Table')
+
     except Error as e:
         print(f'❌ Error: {e}')
 
